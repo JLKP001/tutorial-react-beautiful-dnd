daf3e614b0174738b9c857eaf7be6821f43c91a5
Jonathan Lim <jl14274@my.bristol.ac.uk>
2021-01-04 22:25:42 +0000

    Added Column component using styled-components library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 import { initialData } from "./initialData";
+import { Column } from "./Column";
 
 const App = () => {
   const [state, setstate] = useState(initialData);
-  return <div>hello world</div>;
+  return (
+    <>
+      {state.columnOrder.map((columnId) => {
+        const column = state.columns[columnId];
+        const tasks = column.taskIds.map((taskIds) => state.tasks[taskIds]);
+
+        return <Column key={column.id} column={column} tasks={tasks} />;
+      })}
+    </>
+  );
 };
 
 export default App;
