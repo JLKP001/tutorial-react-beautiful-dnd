8f8b6d24f1668b3a362a00f859d96a20df3e4a94
Jonathan Lim <jl14274@my.bristol.ac.uk>
2021-01-04 22:12:02 +0000

    Created dummy initialData

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { initialData } from "./initialData";
 
 const App = () => {
+  const [state, setstate] = useState(initialData);
   return <div>hello world</div>;
 };
 
