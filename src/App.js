4281966aad66443e333e86c50fe9441f052508b3
Jonathan Lim <jl14274@my.bristol.ac.uk>
2021-01-09 16:51:11 +0000

    Added onDragEnd function to persist reordering

--- src/App.js
+++ src/App.js
@@ -5,13 +5,38 @@ import { Column } from "./Column";
 
 const App = () => {
   const [state, setstate] = useState(initialData);
-  const onDragEnd = () => {};
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = { ...column, taskIds: newTaskIds };
+    const newState = {
+      ...state,
+      columns: { ...state.columns, [newColumn.id]: newColumn },
+    };
+    setstate(newState);
+  };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       {state.columnOrder.map((columnId) => {
         const column = state.columns[columnId];
-        const tasks = column.taskIds.map((taskIds) => state.tasks[taskIds]);
+        const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
 
         return <Column key={column.id} column={column} tasks={tasks} />;
       })}
